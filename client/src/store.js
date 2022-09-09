a600ebe9adb7303cdaf163273f8268f10080c0e5
veselov1209 <veselov1209@gmail.com>
2022-09-09 21:15:14 +0300

    refactor boot

---
+++ client/src/store.js
@@ -0,0 +1,20 @@
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import combineReducers from './reducers/index';
+import rootSaga from './sagas/rootSaga';
+import { initSocket } from './api/ws/socketController';
+import configureStore from './boot/configureStore';
+
+
+const sagaMiddleware = createSagaMiddleware();
+
+  const store = createStore(combineReducers,
+     composeWithDevTools(applyMiddleware(sagaMiddleware)));
+
+  sagaMiddleware.run(rootSaga, store.dispatch);
+  
+  initSocket(store)
+  
+  export default store;
+
